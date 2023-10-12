839d7be6410707854e0ac8098323ccd061742d50
nagchanallen <nagisawags@gmail.com>
2023-10-13 03:12:03 +0800

    feat(web): Setup env variables in vite
    Ref #4
    
    We use firebase config as first step.
    Also improved Makefile operations

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,9 @@
+import Config from './config'
+
 function App() {
   return (
     <div className="flex">
-      <h1 className="text-3xl font-bold underline">Main Page</h1>
+      <h1 className="text-3xl font-bold underline">{JSON.stringify(Config)}</h1>
     </div>
   )
 }
