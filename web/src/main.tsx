89fca916e8b686a5e6ced1277bb296b23618015e
nagchanallen <nagisawags@gmail.com>
2023-10-13 03:25:18 +0800

    feat(web): Setup firebase web SDK
    Ref #4

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { initializeApp } from 'firebase/app'
+import Config from './config'
+
+initializeApp(Config.firebase)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
