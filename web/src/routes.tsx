7f37459e87e2fa3f40a9f7586bc9bf253a81946e
nagchanallen <nagisawags@gmail.com>
2023-10-14 23:17:23 +0800

    feat(web): Scaffold Login Screen
    Ref #4

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,11 +2,18 @@ import { RouteObject } from 'react-router-dom'
 
 import Root from './Root'
 import ErrorScreen from './screens/ErrorScreen'
+import LoginScreen from './screens/Login/LoginScreen'
 
 export const routes: RouteObject[] = [
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorScreen />,
+    children: [
+      {
+        path: 'login',
+        element: <LoginScreen />,
+      },
+    ],
   },
 ]
---
+++ web/src/screens/Login/LoginScreen.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from 'react'
+
+const LoginScreen = (): ReactElement => {
+  return <div>Login Screen</div>
+}
+
+export default LoginScreen
