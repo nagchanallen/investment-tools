8ea093cb137236cc82ed0b8833e6e55a70277a82
nagchanallen <nagisawags@gmail.com>
2023-10-15 23:54:03 +0800

    feat(web): Implement google SSO sign out flow
    Ref #4

--- web/src/screens/HomeScreen.tsx
+++ web/src/screens/HomeScreen.tsx
@@ -1,13 +1,34 @@
-import { ReactElement, useContext } from 'react'
+import { ReactElement, useCallback, useContext } from 'react'
+import { Button } from 'flowbite-react'
+import { useNavigate } from 'react-router-dom'
 
 import { AuthContext } from '../providers/AuthProvider'
 
 const HomeScreen = (): ReactElement => {
-  const { user } = useContext(AuthContext) ?? {}
+  const navigate = useNavigate()
+
+  const { user, signOut } = useContext(AuthContext) ?? {}
+
+  const onSignOutButtonClick = useCallback(async () => {
+    if (!signOut) {
+      // TODO: Handle auth context undefined
+      return
+    }
+
+    try {
+      await signOut?.()
+      navigate('/sign-in')
+    } catch (e) {
+      // TODO: Handle Sign Out Error
+      console.error(e)
+    }
+  }, [navigate, signOut])
+
   return (
     <div>
       <h5>Home</h5>
       <p>{JSON.stringify(user)}</p>
+      <Button onClick={onSignOutButtonClick}>Sign Out</Button>
     </div>
   )
 }
