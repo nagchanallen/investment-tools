684bb88759dc621cd2220991e36f31dc2f49cc64
nagchanallen <nagisawags@gmail.com>
2023-10-21 16:55:41 +0800

    fix(web): Add handling for firebase non-initialized user in route guard
    Ref #14

--- web/src/components/ProtectedRoute.tsx
+++ web/src/components/ProtectedRoute.tsx
@@ -3,9 +3,14 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 import { AuthContext } from '../providers/AuthProvider'
 import { paths } from '../paths'
+import LoadingScreen from '../screens/LoadingScreen'
 
 const ProtectedRoute = (): ReactElement => {
-  const { user } = useContext(AuthContext)
+  const { isInitialized, user } = useContext(AuthContext)
+
+  if (!isInitialized) {
+    return <LoadingScreen />
+  }
 
   if (!user) {
     return <Navigate to={paths.SignIn} replace={true} />
