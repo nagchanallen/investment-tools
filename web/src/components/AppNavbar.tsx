708a0b4d86fc527adbc168335ef834e752d450ae
nagchanallen <nagisawags@gmail.com>
2023-10-21 16:55:52 +0800

    fix(web): Use react router link instead of href props
    Ref #14

--- web/src/components/AppNavbar.tsx
+++ web/src/components/AppNavbar.tsx
@@ -1,6 +1,6 @@
 import { ReactElement, useCallback, useContext } from 'react'
 import { Button, Navbar } from 'flowbite-react'
-import { useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 import { paths } from '../paths'
 import { AuthContext } from '../providers/AuthProvider'
@@ -22,24 +22,25 @@ const AppNavbar = (): ReactElement => {
 
   return (
     <Navbar fluid={true} rounded={true}>
-      <Navbar.Brand href={paths.Home}>
-        <img src="/icon.svg" className="mr-3 h-12" alt="Vite Logo" />
-        <span className="whitespace-nowrap text-xl font-semibold">
-          Investment Tools
-        </span>
-      </Navbar.Brand>
+      <Link to={paths.Home}>
+        <Navbar.Brand>
+          <img src="/icon.svg" className="mr-3 h-12" alt="Vite Logo" />
+          <span className="whitespace-nowrap text-xl font-semibold">
+            Investment Tools
+          </span>
+        </Navbar.Brand>
+      </Link>
       <div className="flex md:order-2">
         {user && <Button onClick={onSignOutButtonClick}>Sign Out</Button>}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
         {user && (
-          <Navbar.Link
-            href={paths.Home}
-            active={location.pathname === paths.Home}
-          >
-            Home
-          </Navbar.Link>
+          <Link to={paths.Home}>
+            <Navbar.Link active={location.pathname === paths.Home}>
+              Home
+            </Navbar.Link>
+          </Link>
         )}
       </Navbar.Collapse>
     </Navbar>
