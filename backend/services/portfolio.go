7a490b56cbe576d452fdd29b4ad68b8e5ae0c2c3
nagchanallen <nagisawags@gmail.com>
2023-10-24 00:33:31 +0800

    feat(backend): Add Create Stock Transaction Operation
    Ref #12
    
    Also update the model and context interface

--- backend/services/portfolio.go
+++ backend/services/portfolio.go
@@ -3,34 +3,34 @@ package services
 import (
 	"context"
 
-	"github.com/google/uuid"
 	models "github.com/nagchanallen/investment-tools/models/portfolio"
 	repositories "github.com/nagchanallen/investment-tools/repositories/portfolio"
 )
 
 type IPortfolioService interface {
-	GetStockTransactions(ctx *context.Context, userId string, page int64, perPage int64, orderBy string) ([]models.StockTransaction, error)
-	CreateStockTransaction(ctx *context.Context, userId string, stockTransaction models.StockTransaction) (uuid.UUID, error)
-	UpdateStockTransaction(ctx *context.Context, userId string, stockTransaction models.StockTransaction) error
-	DeleteStockTransaction(ctx *context.Context, userId string, uuid uuid.UUID) error
+	GetStockTransactions(ctx context.Context, userId string, page int64, perPage int64, orderBy string) ([]models.StockTransaction, error)
+	CreateStockTransaction(ctx context.Context, userId string, stockTransaction models.StockTransaction) error
+	UpdateStockTransaction(ctx context.Context, userId string, stockTransaction models.StockTransaction) error
+	DeleteStockTransaction(ctx context.Context, userId string, transactionId string) error
 }
 
 type PortfolioService struct {
 	StockTransactionRepository repositories.IStockTransactionRepository
 }
 
-func (s *PortfolioService) GetStockTransactions(ctx *context.Context, userId string, page int64, perPage int64, orderBy string) ([]models.StockTransaction, error) {
+func (s *PortfolioService) GetStockTransactions(ctx context.Context, userId string, page int64, perPage int64, orderBy string) ([]models.StockTransaction, error) {
 	return nil, nil
 }
 
-func (s *PortfolioService) CreateStockTransaction(ctx *context.Context, userId string, stockTransaction models.StockTransaction) (uuid.UUID, error) {
-	return uuid.UUID{}, nil
+func (s *PortfolioService) CreateStockTransaction(ctx context.Context, userId string, stockTransaction models.StockTransaction) error {
+	err := s.StockTransactionRepository.CreateStockTransaction(ctx, userId, stockTransaction)
+	return err
 }
 
-func (s *PortfolioService) UpdateStockTransaction(ctx *context.Context, userId string, stockTransaction models.StockTransaction) error {
+func (s *PortfolioService) UpdateStockTransaction(ctx context.Context, userId string, stockTransaction models.StockTransaction) error {
 	return nil
 }
 
-func (s *PortfolioService) DeleteStockTransaction(ctx *context.Context, userId string, uuid uuid.UUID) error {
+func (s *PortfolioService) DeleteStockTransaction(ctx context.Context, userId string, transactionId string) error {
 	return nil
 }
